2a1b2542ccfca18803fd716e63cdb2a1e0b4d6c0
Nicolas <nicolas.sabbatini.95@gmail.com>
2020-09-28 23:23:36 -0300

    Assets and start

--- src/main.lua
+++ src/main.lua
@@ -1,3 +1,21 @@
+require './shared/globals'
+
+local push = require './libs/push'
+
+-- Push set up
+function love.resize(w, h)
+    push:resize(w, h)
+end
+
+love.graphics.setDefaultFilter('nearest', 'nearest')
+
+push:setupScreen(FAKE_WIDTH, FAKE_HEIGHT, REAL_WIDTH, REAL_HEIGHT, {
+    vsync = true,
+    fullscreen = false,
+    resizable = true,
+  })
+--
+
 function love.load()
 end
 
