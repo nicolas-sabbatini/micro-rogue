c415602fd4f6aaffa4394113736be62d72b4f9e0
Nicolas <nico@pop-os.localdomain>
2020-09-28 22:55:56 -0300

    Init

---
+++ src/conf.lua
@@ -0,0 +1,13 @@
+function love.conf(t)
+    t.identity = 'micro-rogue'             -- The name of the save directory (string)
+    t.version = "11.3"                     -- The LÖVE version this game was made for (string)
+
+    t.window.title = "Micro Rogue"         -- The window title (string)
+--    t.window.icon = TODO                 -- Filepath to an image to use as the window's icon (string)
+    t.window.width = 800                   -- The window width (number)
+    t.window.height = 600                  -- The window height (number)
+    t.window.resizable = true              -- Let the window be user-resizable (boolean)
+    t.window.minwidth = 400                -- Minimum window width if the window is resizable (number)
+    t.window.minheight = 300               -- Minimum window height if the window is resizable (number)
+    t.window.vsync = 1                     -- Vertical sync mode (number)
+end
